fb46ccd07dc6d2ba9ff557bd909bdb3202f097ee
Vinicius Correia <115660782+Jsn0770@users.noreply.github.com>
2024-04-29 18:18:46 -0300

    Corrigindo bugs no Front

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,32 +1,15 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState} from 'react'
 import { ShopContext} from '../Context/ShopContext'
-import { useParams } from 'react-router-dom'
-import { getProductById } from '../services/products/products.service'
 import Breadcrums from '../Components/Breadcrums/Breadcrums'
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
 import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
 import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
-const axios = require('axios').default;
+import { useParams } from 'react-router-dom'
 
 export const Product = () => {
-  const [product, setProduct] = useState([]);
-  // const {all_product} = useContext(ShopContext)
-  // const product = all_product.find((e)=> e.id === Number (productId));
-  const { productId } = useParams();
-
-  async function getProduct() {
-    await fetch('http://localhost:4000/products/1' ,{
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-      },
-    }).then((resp) => resp.json()).then((data) => {setProduct(data)});
-  }
-
-  useEffect(() => {
-    getProduct()
-  }, [])
-
+  const {all_product} = useContext(ShopContext)
+  const {productId} = useParams(); 
+  const product = all_product.find((e)=> e.id === Number(productId));
   return (
     <div>
       <Breadcrums product={product}/>
